4a6eb5255d333fb420ee1bfffe3ab3ca3ab3d368
Кирилл Вахлин <kirill.vahlin@gmail.com>
2020-01-30 13:50:02 +0300

    Planing

---
+++ src/components/Main.vue
@@ -0,0 +1,79 @@
+<template>
+	<div id="main" >
+		<!-- Main: начало -->
+		
+		<!-- Main: конец -->
+	</div>
+</template>
+
+<script>
+
+	//
+	//	Главный экран
+	//	
+	
+	export default {
+		
+		//
+		//	Компоненты
+		//
+		
+		components:{
+			
+		},
+		
+		//
+		//	Данные
+		//
+		
+		data: function(){
+			return {
+				
+        	}
+		},
+		//
+		//	--mounted
+		//
+		
+		mounted: function(){
+			
+		},
+		
+		//
+		//	--created
+		//
+		
+		created: function(){
+			
+		},
+		
+		//
+		//	--computed
+		//
+		
+		computed: {
+			
+		},
+		
+		//
+		//	--beforeMount
+		//
+		
+		beforeMount: function(){
+			
+		},
+		
+		//
+		//	--methods
+		//
+		
+		methods: {
+			
+		}
+	
+  }
+</script>
+
+<style>
+
+</style>
